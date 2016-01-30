7a9bd6c053cf3a780470cb86aa5a59fbba75c3dc
Tyler <tyler@tylermakes.com>
2016-01-30 14:27:44 -0500

    adding basic character

---
+++ Enritual_Rae/Assets/TylerAssets/MainCharacterScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCharacterScript : MonoBehaviour {
+
+	public GameObject mainSprite;
+	public Sprite[] sprites;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		print(Input.GetAxis("Vertical"));
+		if (Input.GetAxis("Horizontal") != 0) {
+			mainSprite.GetComponent<SpriteRenderer> ().sprite = sprites [1];
+		} else if (Input.GetAxis("Vertical") != 0) {
+			mainSprite.GetComponent<SpriteRenderer> ().sprite = sprites [1];
+		} else if (Input.GetButton ("Fire1")) {
+			mainSprite.GetComponent<SpriteRenderer> ().sprite = sprites [2];
+		} else if (Input.GetButton ("Fire2")) {
+			mainSprite.GetComponent<SpriteRenderer> ().sprite = sprites [3];
+		} else if (Input.GetButton ("Fire3")) {
+			mainSprite.GetComponent<SpriteRenderer> ().sprite = sprites [4];
+		} else if (Input.GetButton ("Jump")) {
+			mainSprite.GetComponent<SpriteRenderer> ().sprite = sprites [5];
+		} else {
+			mainSprite.GetComponent<SpriteRenderer> ().sprite = sprites [0];
+		}
+	}
+}

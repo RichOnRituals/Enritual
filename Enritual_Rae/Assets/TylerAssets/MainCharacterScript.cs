c4fb4402902e8dcbcc2dde5482382b1b0031cd68
Tyler <tyler@tylermakes.com>
2016-01-30 18:23:04 -0500

    updating states

--- Enritual_Rae/Assets/TylerAssets/MainCharacterScript.cs
+++ Enritual_Rae/Assets/TylerAssets/MainCharacterScript.cs
@@ -9,10 +9,16 @@ public class MainCharacterScript : MonoBehaviour {
 	public int walkAnimationSpeed = 20;
 	private bool walkLeft = true;
 	private int walkWait = 0;
+	public enum PlayerState { y, m, c, a, jump, crouch, neutral, walking };
+	private PlayerState myPlayerState = PlayerState.neutral;
 
 	// Use this for initialization
 	void Start () {
 	
+	}
+
+	public PlayerState GetState() {
+		return myPlayerState;
 	}
 	
 	// Update is called once per frame
@@ -28,17 +34,24 @@ public class MainCharacterScript : MonoBehaviour {
 		if (!walking) {
 			if (Input.GetKey ("1")) {//} else if (Input.GetButton ("Fire1")) {
 				mainSprite.GetComponent<SpriteRenderer> ().sprite = sprites [1];
+				myPlayerState = PlayerState.y;
 			} else if (Input.GetKey ("2")) {//} else if (Input.GetButton ("Fire2")) {
+				myPlayerState = PlayerState.m;
 				mainSprite.GetComponent<SpriteRenderer> ().sprite = sprites [2];
 			} else if (Input.GetKey ("3")) {//} else if (Input.GetButton ("Fire3")) {
+				myPlayerState = PlayerState.c;
 				mainSprite.GetComponent<SpriteRenderer> ().sprite = sprites [3];
 			} else if (Input.GetKey ("4")) {//} else if (Input.GetButton ("Jump")) {
+				myPlayerState = PlayerState.a;
 				mainSprite.GetComponent<SpriteRenderer> ().sprite = sprites [4];
 			} else if (Input.GetKey ("5")) {//} else if (Input.GetButton ("Jump")) {
+				myPlayerState = PlayerState.jump;
 				mainSprite.GetComponent<SpriteRenderer> ().sprite = sprites [5];
 			} else if (Input.GetKey ("6")) {//} else if (Input.GetButton ("Jump")) {
+				myPlayerState = PlayerState.crouch;
 				mainSprite.GetComponent<SpriteRenderer> ().sprite = sprites [6];
 			} else {
+				myPlayerState = PlayerState.neutral;
 				mainSprite.GetComponent<SpriteRenderer> ().sprite = sprites [0];
 			}
 		}
@@ -46,6 +59,7 @@ public class MainCharacterScript : MonoBehaviour {
 
 	void UpdateWalking() {
 		if (!walking) {
+			myPlayerState = PlayerState.walking;
 			walking = true;
 			walkWait = 0;
 		}

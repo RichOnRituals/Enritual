ca91f6f5c707752524c4d2e2d1a1e069a7ce475d
rheitkamp <rae.heitkamp@gmail.com>
2016-01-31 16:54:03 -0500

    Replaces Enritual_MASTER with Enritual_GGJ -- the official submission to the organizers

---
+++ Enritual_GGJ/src/Enritual_MASTER/Assets/MenuAssets/MenuScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+	public void MainMenuPress() {
+		Debug.Log ("The main menu script was called.");
+		Application.LoadLevel(0);
+		Debug.Log("The first level in the build should be called.");
+	}
+	
+	public void PlayPress() {
+		Application.LoadLevel (1);
+	}
+
+	public void CreditsPress() {
+		Application.LoadLevel (2);
+	}
+
+	public void ExitPress() {
+		Application.Quit ();
+	}
+
+}
---
+++ Enritual_GGJ/src/Enritual_MASTER/Assets/RitualSoundSwitch.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RitualSoundSwitch : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> target_audio;
+    [SerializeField]
+    private GameObject background_music;
+
+	// Use this for initialization
+    void OnTriggerEnter(Collider coll)
+    {
+        foreach(GameObject sounds in target_audio)
+        {
+            sounds.SetActive(true);
+            background_music.SetActive(false);
+        }
+        Debug.Log("Player has entered the town");
+    }
+
+    void OnTriggerExit(Collider coll)
+    {
+        foreach (GameObject sounds in target_audio)
+        {
+            sounds.SetActive(false);
+            background_music.SetActive(true);
+        }
+    }
+}
